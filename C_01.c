0d40dda019760c82d6395a2e96ed1ec2ac5674b5
itagusu <88267436+itagusu@users.noreply.github.com>
2022-01-10 17:24:19 +0900

    C言語計算

--- C_01.c
+++ C_01.c
@@ -2,6 +2,13 @@
 
 int main(void)
 {
-  printf("hello, world\n");
+  printf("こんにちは。板楠です\n");
+  printf("今年28歳になります\n");
+  printf("よろしくお願いいたします\n");
+  printf("29+29の計算をします\n");
+  printf("%d\n",29+29);
+  printf("%d\n",10-5);
+  int x = -6;
+  printf("%d\n",x*3);
   return 0;
 }
