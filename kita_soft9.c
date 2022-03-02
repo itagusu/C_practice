998713a53afc659ec8b8fada50f13dac4886279c
itagusu <88267436+itagusu@users.noreply.github.com>
2022-03-02 16:37:52 +0900

    9_4

--- kita_soft9.c
+++ kita_soft9.c
@@ -1,17 +1,32 @@
 #include <stdio.h>
 
-extern int sub9(int);
+extern int sub9_1(int);
+extern int sub9_2(int, int);
 
 int main()
 {
-  int a = sub9(10);
+  /*
+  int a = sub9_1(10);
   int b = a + 10;
   printf("%d\n", b);
+  */
+  printf("数字を入力\n");
+  int a;
+  int b;
+  scanf("%d", &a);
+  scanf("%d", &b);
+  printf("%d\n", sub9_2(a, b));
   return 0;
 }
 
-int sub9(int x)
+int sub9_1(int x)
 {
   int z = x * x;
+  return z;
+}
+
+int sub9_2(int x, int y)
+{
+  int z = (x + y) / 2;
   return z;
 }
---
+++ kita_soft9_4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+extern int triangle(int);
+
+int main()
+{
+  triangle(3);
+  triangle(4);
+  int a;
+  printf("好きな数字を入力\n");
+  scanf("%d", &a);
+  triangle(a);
+  return 0;
+}
+
+int triangle(int size)
+{
+  int i;
+  int j;
+  for (i = 0; i < size; i++)
+  {
+    for (j = 0; j <= i; j++)
+    {
+      printf("悠");
+    }
+    printf("\n");
+  }
+  printf("\n");
+  return size;
+}
